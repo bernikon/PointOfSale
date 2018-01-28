eec91b776030bcf93bb2db2c1f454a612fb35484
Piotr <kondrat.piotr@gmail.com>
2018-01-28 15:08:34 +0100

    point of sale project

--- src/main/java/pl/pk/pointofsale/controllers/Controller.java
+++ src/main/java/pl/pk/pointofsale/controllers/Controller.java
@@ -7,6 +7,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import pl.pk.pointofsale.models.MysqlConnector;
+import pl.pk.pointofsale.models.dao.ProductDao;
+import pl.pk.pointofsale.models.dao.impl.ProductDaoImpl;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -20,11 +22,18 @@ public class Controller implements Initializable {
     @FXML
     TextField textBarcode, textLCD;
 
+    private ProductDao productDao = new ProductDaoImpl();
 
     public void initialize(URL location, ResourceBundle resources) {
-        MysqlConnector.getInstance();
+        // MysqlConnector.getInstance();
+        buttonEnter.setOnMouseClicked(e -> showProduct());
 
 
+    }
+
+    private void showProduct() {
+        String barcode = textBarcode.getText();
+ // ...
 
     }
 }
